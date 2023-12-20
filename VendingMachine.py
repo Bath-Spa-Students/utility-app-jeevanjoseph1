0801d60584a28b5fb5e174d810c2f88ad8ab83f9
jeevanjoseph1 <147370893+jeevanjoseph1@users.noreply.github.com>
2023-12-20 10:43:12 +0400

    VendingMachine.py

--- VendingMachine.py
+++ VendingMachine.py
@@ -20,13 +20,13 @@ class VendingMachine:
         self.items = {'1': {'name': '7up', 'price': 2, 'quantity': 10},
                       '2': {'name': 'Chips', 'price': 1.50, 'quantity': 8},
                       '3': {'name': 'Chocolate', 'price': 3.50, 'quantity': 15},
-                      '4':{'name':'sandwhich','price':3.00,'quantity':5},
-                      '5':{'name':'water','price':1.00,'quantity':40},
-                      '6':{'name':'break','price':0.50,'quantity':20},
-                      '7':{'name':'cookie','price':1.50,'quantity':10},
-                      '8':{'name':'KitKat','price':1.50,'quantity':8},
-                      '9':{'name':'protein bar','price':1.00,'quantity':25},
-                      '10':{'name':'candy','price':1.25,'quantity':20}}
+                      '4': {'name': 'sandwich', 'price': 3.00, 'quantity': 5},
+                      '5': {'name': 'water', 'price': 1.00, 'quantity': 40},
+                      '6': {'name': 'break', 'price': 0.50, 'quantity': 20},
+                      '7': {'name': 'cookie', 'price': 1.50, 'quantity': 10},
+                      '8': {'name': 'KitKat', 'price': 1.50, 'quantity': 8},
+                      '9': {'name': 'protein bar', 'price': 1.00, 'quantity': 25},
+                      '10': {'name': 'candy', 'price': 1.25, 'quantity': 20}}
         self.balance = 0
 
     def display_items(self):
@@ -45,6 +45,24 @@ class VendingMachine:
                 self.balance -= item['price']
                 item['quantity'] -= 1
                 print(f"Purchased {item['name']} for ${item['price']:.2f}. Remaining Balance: ${self.balance:.2f}")
+
+                # Ask if the user wants to purchase anything else
+                while True:
+                    purchase_more = input("Do you want to purchase anything else? (yes/no): ").lower()
+                    if purchase_more == 'no':
+                        print("""
+████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗  ██╗░░░██╗░█████╗░██╗░░░██╗  ██╗    ██╗░░
+╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝  ╚██╗░██╔╝██╔══██╗██║░░░██║  ╚═╝    ╚██╗░
+░░░██║░░░███████║███████║██╔██╗██║█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║  ░░░    ░╚██╗
+░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░  ░░╚██╔╝░░██║░░██║██║░░░██║  ░░░    ░██╔╝
+░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗  ░░░██║░░░╚█████╔╝╚██████╔╝  ██╗    ██╔╝░
+░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚═╝    ╚═╝░░""")
+                        return  # Exit the purchase_item method
+                    elif purchase_more == 'yes':
+                        self.display_items()  # Display items again
+                        return  # Exit the purchase_item method
+                    else:
+                        print("Invalid choice. Please enter 'yes' or 'no'.")
             else:
                 print("Insufficient balance or item out of stock.")
         else:
@@ -54,6 +72,7 @@ class VendingMachine:
         if self.balance > 0:
             print(f"Returning change: ${self.balance:.2f}")
             self.balance = 0
+
 
 def main():
     vending_machine = VendingMachine()
@@ -80,17 +99,12 @@ def main():
         elif choice == '4':
             vending_machine.return_change()
         elif choice == '5':
-            exit = input("""""
-████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗  ██╗░░░██╗░█████╗░██╗░░░██╗  ██╗    ██╗
-╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝  ╚██╗░██╔╝██╔══██╗██║░░░██║  ╚═╝    ╚██╗
-░░░██║░░░███████║███████║██╔██╗██║█████═╝░  ░╚████╔╝░██║░░██║██║░░░██║  ░░░    ░╚██╗
-░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░  ░░╚██╔╝░░██║░░██║██║░░░██║  ░░░    ░██╔╝
-░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗  ░░░██║░░░╚█████╔╝╚██████╔╝  ██╗    ██╔╝
-░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚═╝    ╚═╝""")
+            print("")
             break
         else:
+
             print("Invalid choice. Please try again.")
-            
+
 
 if __name__ == "__main__":
     main()
